77ca015eb823ecda4fa054e49b576ac066cc3ba7
Vineeth T P <Vineethav1333@gmail.com>
2026-06-17 15:01:31 +0530

    Load and preview Titanic dataset from CSV
    Added code to read and display Titanic dataset.

--- Titanic.py
+++ Titanic.py
@@ -5,4 +5,15 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+#Reading the csv file into a variable
+titanic = pd.read_csv("./train_and_test2.csv")
+
+#Viewing the 1st five rows
+titanic.head()
+
+#Viewing the last 5 rows
+titanic.tail()
+
+#Getting more info about the rows
+titanic.info()
 

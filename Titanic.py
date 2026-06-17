df72cbaa11194648f6b8c161004ebe04937efcd0
Vineeth T P <Vineethav1333@gmail.com>
2026-06-17 16:10:19 +0530

    Add seaborn and sklearn imports to Titanic.py

--- Titanic.py
+++ Titanic.py
@@ -4,6 +4,11 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+
 
 #Reading the csv file into a variable
 titanic = pd.read_csv("./train_and_test2.csv")
